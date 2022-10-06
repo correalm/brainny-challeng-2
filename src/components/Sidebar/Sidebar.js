6e20b2e9fcf40e3e31606039792fac42417e15f8
miguel <luismigueldasilva.99@gmail.com>
2022-10-06 17:26:04 -0300

    refac: css on card

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './Sidebar.scss'
 import SidebarOption from './SidebarOption/SidebarOption'
 import arrow from '../../assets/arrow.svg'
