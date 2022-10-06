b72a3450f5c04a760e47e5132cd8d0a6d1a0ff83
miguel <luismigueldasilva.99@gmail.com>
2022-10-05 23:37:12 -0300

    💄: refac svgs

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,7 +4,10 @@ import SidebarOption from './SidebarOption/SidebarOption'
 import arrow from '../../assets/arrow.svg'
 import admin_avatar from '../../assets/admin_avatar.svg'
 import client_avatar from '../../assets/client_avatar.svg'
-import workers_avatar from '../../assets/workers_avatar.svg' 
+import workers_avatar from '../../assets/workers_avatar.svg'
+import    admin_avatar_selected from   '../../assets/admin_avatar_selected.svg'
+import   client_avatar_selected from  '../../assets/client_avatar_selected.svg'
+import  workers_avatar_selected from '../../assets/workers_avatar_selected.svg'  
 
 
 const Sidebar = () => {
@@ -25,9 +28,27 @@ const Sidebar = () => {
       <div className="sidebar">
         <div className='content'>
           <div className="content content-options">
-            <SidebarOption id='1' text='Administradores' img={admin_avatar}   selected={selected} onClickFunction={ () => toggleSelected('1') }/>
-            <SidebarOption id='2' text='Clientes'        img={client_avatar}  selected={selected} onClickFunction={ () => toggleSelected('2') } />
-            <SidebarOption id='3' text='Colaboradores'   img={workers_avatar} selected={selected} onClickFunction={ () => toggleSelected('3') } />
+            <SidebarOption 
+              id='1' 
+              text='Administradores' 
+              img={ selected && selected.id === '1' ? admin_avatar_selected   : admin_avatar }   
+              selected={ selected } 
+              onClickFunction={ () => toggleSelected('1') }
+            />
+            <SidebarOption 
+              id='2' 
+              text='Clientes'        
+              img={ selected && selected.id === '2' ? client_avatar_selected  : client_avatar }  
+              selected={ selected } 
+              onClickFunction={ () => toggleSelected('2') } 
+            />
+            <SidebarOption 
+              id='3' 
+              text='Colaboradores'   
+              img={ selected && selected.id === '3' ? workers_avatar_selected : workers_avatar } 
+              selected={ selected } 
+              onClickFunction={ () => toggleSelected('3') } 
+            />
           </div>
           <div className="content content-footer">
             <div className='button-footer'>
--- src/components/Sidebar/SidebarOption/SidebarOption.js
+++ src/components/Sidebar/SidebarOption/SidebarOption.js
@@ -6,7 +6,7 @@ const SidebarOption = ({text, img, id, onClickFunction, selected}) => {
   return (
     <>
     <div className='wrapper' onClick={ () => onClickFunction() }>
-      {selected && selected.id === id ? <div className='fill'></div> : null}
+      {selected && selected.id === id && <div className='fill'></div>}
       <div id={id} className= 'option' >
         <img src={ img } alt='avatar'/>
         <p>{ text }</p>
